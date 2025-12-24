f3cc7c8c67895545d227c3a1e739cd4d60e8c281
Yasumasa Maji <keiri@misky.co.jp>
2025-12-25 05:20:47 +0900

    サーバー設定変更テスト

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <main>
       <h1>The Gentle Light</h1>
-      <p>Coming soon. (staging test next.js)</p>
+      <p>Coming soon. (staging test next.js3)</p>
       <p><a href="/admin/">Admin</a></p>
     </main>
   )
