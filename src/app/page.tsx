d45d826d6d959dd1d1d70a5c936755111bc1ef2e
Yasumasa Maji <keiri@misky.co.jp>
2025-12-26 03:59:38 +0900

    環境調整中2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
       <h1>The Gentle Light</h1>
       <p>Coming soon. (staging test next.js3)</p>
       <p><a href="/admin/">Admin</a></p>
-      <p><a href="/test/">基礎構築テスト</a></p>
+      <p><a href="/test/">基礎構築テスト2</a></p>
     </main>
   )
 }
