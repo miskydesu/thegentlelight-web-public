7ddd389d9e66af222f7e19dec6fc3979a8963ee7
Yasumasa Maji <keiri@misky.co.jp>
2026-01-07 06:00:50 +0900

    サイトマップ-エラー修正

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -14,7 +14,6 @@ export default function robots(): MetadataRoute.Robots {
         },
       ],
       sitemap: `${base}/sitemap.xml`,
-      host: base,
     }
   }
 
@@ -26,7 +25,6 @@ export default function robots(): MetadataRoute.Robots {
       },
     ],
     sitemap: `${base}/sitemap.xml`,
-    host: base,
   }
 }
 
